288d223fcac1d089ff99623d8bce11f49ff0e3c7
Sophie Francis <Sophie.Francis@serko.com>
2021-12-23 10:10:30 +1300

    randomiser added

--- src/components/gameRPS/RobotPlayer.tsx
+++ src/components/gameRPS/RobotPlayer.tsx
@@ -3,10 +3,20 @@ import React, { ReactElement } from "react";
 
 interface Props {}
 
+const playItems = new Map([
+  [1, "rock"],
+  [2, "paper"],
+  [3, "scissors"],
+]);
+const min = 1;
+const max = 4;
 export default function RobotPlayer({}: Props): ReactElement {
+  const random = Math.floor(min + Math.random() * (max - min));
   return (
     <div>
-      <Typography>2nd Player</Typography>
+      <Typography>
+        2nd Player {random} {playItems.get(random)}
+      </Typography>
     </div>
   );
 }
